a05d71f01f6d8e2763d7614ad1e3b3c1032d39f9
Jasper Oudemans <jasperoudemans@gmail.com>
2022-02-24 13:57:29 -0700

    changed toy listings page to have gaps between cards

--- client/src/components/Toy.js
+++ client/src/components/Toy.js
@@ -1,25 +1,40 @@
-import React, { useState } from 'react';
+import { TypeOrFieldNameRegExp } from "@apollo/client/cache/inmemory/helpers";
+import React, { useState } from "react";
 
 const cardStyle = {
-    width: "18rem",
-    border: "solid black 1px"
-}
+  width: "18rem",
+  border: "solid rgb(199, 199, 199) 2px",
+  display: "flex",
+  overflow: "hidden",
+  borderRadius: "5px",
+};
 
 const imgStyle = {
-    height: "300px",
-    width: "17.9rem"
-}
-
-function Toy({name, imageURL, price, owner, description, showToyModal}) {
-    return (
-        <div className="card h-100 shadow-lg toyCard" style={cardStyle} onClick={() => showToyModal(name, imageURL, price, owner, description)}>
-                <img className="card-img-top cardImage" src={imageURL} style={imgStyle} alt="Auto Parts Website"/>
-                <div className="card-body">
-                    <h5 className="card-title">{name}</h5>
-                    <h5 className="card-title">{price}</h5>
-                </div>
-            </div>
-    )
+  width: "18rem",
+  borderRadius: "2px",
+};
+//shadow-lg h-100 are classes I took out of top div
+function Toy({ name, imageURL, price, owner, description, showToyModal }) {
+  return (
+    <div
+      className="card toyCard"
+      style={cardStyle}
+      onClick={() => showToyModal(name, imageURL, price, owner, description)}
+    >
+      <div className="polaroid">
+        <img
+          className="card-img-top cardImage"
+          src={imageURL}
+          style={imgStyle}
+          alt="Auto Parts Website"
+        />
+      </div>
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <h5 className="card-title">{price}</h5>
+      </div>
+    </div>
+  );
 }
 
 export default Toy;
