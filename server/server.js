2a1f472c7ede41f1855510f03c1804bc90a455c0
David Schepis <davidschepis@gmail.com>
2022-02-24 11:59:25 -0700

    added apollo to app.js, server.js

--- server/server.js
+++ server/server.js
@@ -5,11 +5,15 @@ const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
+const { authMiddleware } = require('./utils/auth');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
+  playground: true
 });
 
 app.use(express.urlencoded({ extended: false }));
@@ -28,7 +32,7 @@ app.get('/', (req, res) => {
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
-  
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
@@ -36,7 +40,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
     })
   })
 };
-  
+
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
- 
+
