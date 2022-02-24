368a86d785f95f7d0fe8e540304b6eeb35d31073
Jasper Oudemans <jasperoudemans@gmail.com>
2022-02-24 11:37:44 -0700

    started footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/Nav";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 // import Splash from "./components/Splash";
 import Listings from "./components/Listings";
 
@@ -11,8 +11,8 @@ function App() {
       <div>
         <Nav />
         {/* <Splash/> */}
-        {/* <Footer /> */}
-        <Listings/>
+        <Listings />
+        <Footer />
       </div>
     </Router>
   );
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,1 +1,12 @@
-//todo
+const Footer = () => {
+  return (
+    <footer className="zfooter">
+      <ul>
+        <li className="appTitle">ToySRC</li>
+        <li className="footerItem">Hello footer</li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
