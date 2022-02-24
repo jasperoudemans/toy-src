06612e1036643414064055317383bc68bb205eac
Jasper Oudemans <jasperoudemans@gmail.com>
2022-02-24 11:16:35 -0700

    fixed the nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav className="nav">
+    <nav className="znav">
       <ul className="flex">
         <li className="appTitle">ToySRC</li>
         <li className="navBtn">
