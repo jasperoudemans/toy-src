4fbdfc51cae466030fcb27f03ad271872b82b7ff
David Schepis <davidschepis@gmail.com>
2022-02-25 10:40:02 -0700

    added reputation and hasreview to user model, updated seeds

---
+++ client/src/components/Users.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+import users from "./usersData.json";
+
+function Users() {
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [location, setLocation] = useState();
+    const [reputation, setReputation] = useState();
+
+    
+};
+
+export default Users;
