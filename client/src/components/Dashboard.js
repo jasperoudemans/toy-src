6e5d31dd8958870a27c7e9f47aaa2590f506f361
Jasper Oudemans <jasperoudemans@gmail.com>
2022-03-01 13:28:03 -0700

    finished styling comments section of dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,7 +3,7 @@ import { REMOVE_TOY, CHECK_COMMENT } from "../utils/mutations";
 import { QUERY_ME, GET_TOYS } from "../utils/queries";
 import { useQuery, useMutation } from "@apollo/client";
 import "../dashboard.css";
-import EditProfile from "../components/EditProfile"
+import EditProfile from "../components/EditProfile";
 import { Nav, Modal, Tab } from "react-bootstrap";
 
 import checkSRC from "../img/check.png";
@@ -20,19 +20,15 @@ const imgStyle = {
   borderRadius: "2px",
 };
 
-
 const checkStyle = {
   height: "40px",
   width: "40px",
 };
 
-
-
 function Dashboard() {
   const [checkComment] = useMutation(CHECK_COMMENT);
   const [removeToy] = useMutation(REMOVE_TOY);
   const [showEditModal, setEditModal] = useState(false);
-
 
   const deleteToy = (data) => {
     console.log(data);
@@ -53,7 +49,6 @@ function Dashboard() {
     }
     return false;
   };
-
 
   const handleComplete = (toy, comment) => {
     checkComment({
@@ -79,31 +74,34 @@ function Dashboard() {
       <div>
         {toyList.map((e) => (
           <div key={e._id}>
-            <hr></hr>
+            <br />
             {e.name}
+            <hr className="rule" />
             {e.comments.map((c) => (
               <div key={e._id + c._id}>
                 <div className="row" key={c._id}>
-                  <div className="col">
-                    <p>
-                      {c.author}: {c.comment}
-                    </p>
-                  </div>
-                  <div className="col">
-                    {!c.checked ? (
-                      <button
-                        className="proBtn"
-                        onClick={() => handleComplete(e, c)}
-                      >
-                        Complete
-                      </button>
-                    ) : (
-                      <img
-                        src={checkSRC}
-                        alt="checkmark"
-                        style={checkStyle}
-                      ></img>
-                    )}
+                  <div className="flex red">
+                    <div className="">
+                      <p>
+                        {c.author}: {c.comment}
+                      </p>
+                    </div>
+                    <div className="">
+                      {!c.checked ? (
+                        <button
+                          className="proBtn"
+                          onClick={() => handleComplete(e, c)}
+                        >
+                          Complete
+                        </button>
+                      ) : (
+                        <img
+                          src={checkSRC}
+                          alt="checkmark"
+                          style={checkStyle}
+                        ></img>
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
@@ -114,48 +112,51 @@ function Dashboard() {
     );
   };
 
-
-
   return (
     <section className="main">
       <div className="sideWays">
         <div className="nameCard" style={cardStyle}>
           <h1 className="">Welcome, {user.data?.me.username}</h1>
 
-
-          <button className="proBtn" onClick={() => window.location.replace("/addListing")}>Add Listing</button>
-          <button className="proBtn" eventkey="EditProfile"
-          onClick={() =>setEditModal(true)}
-          >Edit Profile</button>
-
+          <button
+            className="proBtn"
+            onClick={() => window.location.replace("/addListing")}
+          >
+            Add Listing
+          </button>
+          <button
+            className="proBtn"
+            eventkey="EditProfile"
+            onClick={() => setEditModal(true)}
+          >
+            Edit Profile
+          </button>
         </div>
         <Modal
-            size="lg"
-            show={showEditModal}
-            onHide={() => setEditModal(false)}
-            aria-labelledby="edit-modal"
-          >
-            <Tab.Container defaultActiveKey="EditProfile">
-              <Modal.Header closeButton>
-                <Modal.Title id="edit-modal">
-                  <Nav variant="pills">
-                    <Nav.Item>
-                      <Nav.Link eventKey="EditProfile">Edit Profile</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                </Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <Tab.Content>
-                  <Tab.Pane eventKey="EditProfile">
-                    <EditProfile
-                      handleModalClose={() => setEditModal(false)}
-                    />
-                  </Tab.Pane>
-                </Tab.Content>
-              </Modal.Body>
-            </Tab.Container>
-          </Modal>
+          size="lg"
+          show={showEditModal}
+          onHide={() => setEditModal(false)}
+          aria-labelledby="edit-modal"
+        >
+          <Tab.Container defaultActiveKey="EditProfile">
+            <Modal.Header closeButton>
+              <Modal.Title id="edit-modal">
+                <Nav variant="pills">
+                  <Nav.Item>
+                    <Nav.Link eventKey="EditProfile">Edit Profile</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Tab.Content>
+                <Tab.Pane eventKey="EditProfile">
+                  <EditProfile handleModalClose={() => setEditModal(false)} />
+                </Tab.Pane>
+              </Tab.Content>
+            </Modal.Body>
+          </Tab.Container>
+        </Modal>
 
         <div className="nameCard" style={cardStyle}>
           <h3>My Info:</h3>
@@ -171,7 +172,6 @@ function Dashboard() {
         <h3>Your Toy Listings:</h3>
       </div>
 
-      
       {toys.data?.toys.map((e) => {
         if (checkUser(e.owner)) {
           return (
