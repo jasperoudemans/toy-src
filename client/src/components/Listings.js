396d3b968f1e698030314e95a6ec08a3928b6fe3
ivibarros <ivibarros19@gmail.com>
2022-03-01 10:04:20 -0700

    comments fix2

--- client/src/components/Listings.js
+++ client/src/components/Listings.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Modal } from "react-bootstrap";
 import Toy from "./Toy";
-import Auth from "../utils/auth";
 
 import { GET_TOYS } from "../utils/queries";
 import { useQuery } from '@apollo/client';
@@ -53,10 +52,6 @@ function Listings() {
       })
       setComments([ ...comments, newComment ])
       setCommentText('')
-      // const toy = listings.find(x => x._id === toyId)
-      // console.log('toy', toy)
-      // toy.comments = [ ...comments, newComment ]
-      // console.log('toy 2', toy)
     }
     catch (e) {
       console.error(error);
@@ -75,8 +70,6 @@ function Listings() {
       const newComments = [...comments]
       newComments.splice(index, 1)
       setComments([ ...newComments ])
-      const toy = listings.find(x => x._id === toyId)
-      toy.comments = newComments
     }
     catch (error) {
       console.error(error);
@@ -86,6 +79,7 @@ function Listings() {
 
   const closeToyModal = () => {
     setModal(false);
+    window.location.assign('/#findtoys');
   };
 
   const showToyModal = (name, imageURL, price, owner, description, comments, toyId) => {
