c964fbe83e96bb524dc666d546a62123cdc3e1ea
Jasper Oudemans <jasperoudemans@gmail.com>
2022-10-02 14:58:30 -0400

    deployed to fly

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,26 +1,26 @@
-const db = require('../config/connection');
-const { User, Toys } = require('../models');
+require("dotenv").config();
+const db = require("../config/connection");
+const { User, Toys } = require("../models");
 
-const usersData = require('./usersData.json');
-const toysData = require('./toysData.json');
+const usersData = require("./usersData.json");
+const toysData = require("./toysData.json");
 
 // TODO
 // Please, remove the deleteMany() method when the project is done to prevent data loss
 
-db.once('open', async () => {
-
+db.once("open", async () => {
   await User.deleteMany({});
   for (const user of usersData) {
-    await User.create(user)
+    await User.create(user);
   }
-  console.log('Users seeded!');
+  console.log("Users seeded!");
 
   await Toys.deleteMany({});
   for (const toy of toysData) {
-    await Toys.create(toy)
+    await Toys.create(toy);
   }
 
-  console.log('Toys seeded!');
+  console.log("Toys seeded!");
 
   process.exit(0);
 });
